555595a86ad4d2e223f044d95860f40bd66198a4
hooman-pro <hooman.programmer@gmail.com>
2017-10-16 09:50:28 +0330

    make folder dynamic
    with this code if you want you can set dir in connectorRoute 
    like:
    'connectorRoute' => ['el-finder/connector' , 'dir'=>'doctor-'.$doctorModel->id],
    
    so it make a folder for each doctor.

--- ConnectorAction.php
+++ ConnectorAction.php
@@ -13,6 +13,11 @@ class ConnectorAction extends Action
     public function run()
     {
         require_once(dirname(__FILE__) . '/php/elFinder.class.php');
+        $this->settings['URL'] = $this->settings['URL'].$_GET['dir'].'/';
+        $this->settings['root'] = $this->settings['root'].$_GET['dir'].'/';
+        if(!file_exists($this->settings['root'])){
+            mkdir($this->settings['root'], 0777, true);
+        }
         $fm = new \elFinder($this->settings);
         $fm->run();
     }
